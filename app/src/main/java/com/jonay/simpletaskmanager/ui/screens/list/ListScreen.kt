653908e5a342a3171a859879e10df2ee849dffbe
Jonay Santana <nookflare@gmail.com>
2025-08-24 15:08:49 +0100

    Remove unnecessarycode and update some library

--- app/src/main/java/com/jonay/simpletaskmanager/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/jonay/simpletaskmanager/ui/screens/list/ListScreen.kt
@@ -1,15 +1,9 @@
 package com.jonay.simpletaskmanager.ui.screens.list
 
-import android.graphics.Color
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.jonay.simpletaskmanager.R
@@ -17,7 +11,6 @@ import com.jonay.simpletaskmanager.common.extensions.Pixel7ProPreview
 import com.jonay.simpletaskmanager.ui.components.base.BaseScreen
 import com.jonay.simpletaskmanager.ui.components.getDimenResource
 import com.jonay.simpletaskmanager.ui.components.getStringResource
-import com.jonay.simpletaskmanager.ui.theme.Black60
 
 /***
  * Created by Jonay Adrián Santana González on 10/08/2025.
